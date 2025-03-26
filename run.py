84db9320e3e008630a4bb2aed4bdc95116e560b7
Vlad Alexan <vlad@onboard-platform.com>
2025-03-26 19:38:14 +0100

    Add storing and retrieval of summary data about opponent behaviour functionality.

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ settings = {
             "parameters": {"storage_dir": "agent_storage/DreamTeam109Agent"},
         },
         {
-            "class": "agents.template_agent.template_agent.TemplateAgent",
-            "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+            "class": "agents.first_agent.first_agent.FirstAgent",
+            "parameters": {"storage_dir": "agent_storage/FirstAgent"},
         },
     ],
     "profiles": ["domains/domain00/profileA.json", "domains/domain00/profileB.json"],
